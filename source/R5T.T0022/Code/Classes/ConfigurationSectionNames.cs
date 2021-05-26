345d7167a1c7fd44543db4f17676d4a91ed5f4d2
David Coats <david@getsafetycone.com>
2021-05-26 11:27:08 -0700

    Add IConfigurationSectionNames extension methods base interface and provide a simple implementation.

---
+++ source/R5T.T0022/Code/Classes/ConfigurationSectionNames.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+
+namespace R5T.T0022
+{
+    /// <summary>
+    /// Simple implementation of <see cref="IConfigurationSectionNames"/>.
+    /// Non-static to allow use as an extension methods base.
+    /// </summary>
+    public class ConfigurationSectionNames : IConfigurationSectionNames
+    {
+        #region Static
+
+        public static ConfigurationSectionNames Instance { get; } = new ConfigurationSectionNames();
+
+        #endregion
+    }
+}
