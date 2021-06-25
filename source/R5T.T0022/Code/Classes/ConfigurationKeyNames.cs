37c3fd5190412197ffddb8f64d904e51b29b26f6
David Coats <david@getsafetycone.com>
2021-06-24 18:57:06 -0700

    Add IConfigurationKeyNames.

---
+++ source/R5T.T0022/Code/Classes/ConfigurationKeyNames.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+
+namespace R5T.T0022
+{
+    public class ConfigurationKeyNames : IConfigurationKeyNames
+    {
+        #region Static
+
+        public static ConfigurationKeyNames Instance { get; } = new();
+
+        #endregion
+    }
+}
---
+++ source/R5T.T0022/Code/Interfaces/IConfigurationKeyNames.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+
+namespace R5T.T0022
+{
+    /// <summary>
+    /// Empty interface to service as extension method base.
+    /// </summary>
+    public interface IConfigurationKeyNames
+    {
+    }
+}
